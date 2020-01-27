01050396e6d5647fae79eebf34b64f9565dad973
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2020-01-27 12:15:53 -0500

    push assignment 02 code onto github

---
+++ RESTfulWebService/src/main/java/com/csye6225/webservice/RESTfulWebService/Validation/PasswordValidator.java
@@ -0,0 +1,22 @@
+package com.csye6225.webservice.RESTfulWebService.Validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_+=]).{8,40})";
+
+    public PasswordValidator() {
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+    }
+
+    public boolean isValid(final String password) {
+
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+
+    }
+}

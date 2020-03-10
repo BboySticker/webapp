bd609d3cb0bd26bf40ee01dbb65d86e863670e73
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2020-03-10 13:23:55 -0400

    update codedeploy related scripts

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -2,3 +2,15 @@
 
 # Fixing permission and attach ownership to tomcat
 # Not 777
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /var/lib/tomcat8 tomcat
+export CATALINA_HOME=/var/lib/tomcat8
+
+cd /var/lib/tomcat8
+# set proper permissions
+sudo chgrp -R tomcat conf
+sudo chmod g+rwx conf
+sudo chmod g+r conf/*
+
+# set ownerships
+sudo chown -R tomcat work/ logs/ webapps/

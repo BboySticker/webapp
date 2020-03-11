f888dad38527c36f7e62a555c793392dd383de5c
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2020-03-11 18:11:03 -0400

    modified codedeploy hooks

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -23,6 +23,10 @@ chown webapp-user:webapp-user /var/webapp/ROOT.jar
 chmod 500 /var/webapp/ROOT.jar
 #chattr +i /var/sample-app/sample-app.jar
 
+if [[ -f /etc/init.d/webapp ]]; then
+    sudo rm /etc/init.d/webapp
+fi
+
 # create symlink to init.d
 ln -s /var/webapp/ROOT.jar /etc/init.d/webapp
 update-rc.d webapp defaults

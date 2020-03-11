05b0ccf7f64382e803616f14c93831537cae310b
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2020-03-11 14:10:12 -0400

    modified codedeploy hooks

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #sudo service tomcat8 start
 cd ~/
+echo "FIRST: setsid java -jar -Dspring.profiles.active=prod ROOT.jar &"
 setsid java -jar -Dspring.profiles.active=prod ROOT.jar &
+echo "SECOND: setsid java -jar -Dspring.profiles.active=prod ROOT.jar &"

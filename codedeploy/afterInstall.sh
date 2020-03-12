4a250f7d804ceb1351fa633621d7164e705dab62
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2020-03-11 23:02:33 -0400

    modified codedeploy hooks

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -21,6 +21,7 @@ sudo chown webapp-user:webapp-user /var/webapp/ROOT.jar /var/webapp/ROOT.conf
 
 # protect application from modifications
 sudo chmod 555 /var/webapp/ROOT.jar
+sudo chmod 555 /var/webapp/ROOT.conf
 #chattr +i /var/sample-app/sample-app.jar
 
 if [[ -f /etc/init.d/webapp ]]; then

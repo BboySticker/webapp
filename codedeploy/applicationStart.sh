f03595da43e0d09c393e5d13564f06a084480547
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2020-03-14 22:06:19 -0400

    update codedeploy related scripts

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -6,7 +6,7 @@ sudo service webapp restart
 echo "Service started."
 
 # running cloud watch agent
-cp /home/ubuntu/cloudwatch-config.json /opt
+sudo cp /home/ubuntu/cloudwatch-config.json /opt
 sudo systemctl enable amazon-cloudwatch-agent.service
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
 sudo systemctl restart amazon-cloudwatch-agent
